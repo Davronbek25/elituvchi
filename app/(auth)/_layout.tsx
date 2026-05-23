6741f1336eae8a39c75976d4a09c07e8d8106a78
Davronbek25 <7521955@studenti.unige.it>
2026-05-23 15:48:07 +0200

    final commit from asus tuf f15 gaming laptop

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
+import CustomButton from '@/components/CustomButton'
+import CustomInput from '@/components/CustomInput'
+import { images } from '@/constants'
+import { Slot } from 'expo-router'
+import { View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const _layout = () => {
+export default function _Layout() {
   return (
-    <View>
-      <Text>Auth layout</Text>
-    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className='bg-white h-full' keyboardShouldPersistTaps='handled'>
+        <View className='w-full relative' style={{ height: Dimensions.get('screen').height / 2.25 }}>
+          <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch' />
+          <Image source={images.logo} className='self-center size-48 absolute -bottom-16 z-10' />
+        </View>
+
+        <CustomInput 
+          placeholder='Enter your email'
+          value={''}
+          onChangeText={() => {}}
+          label='Email'
+          keyboardType='email-address'
+        />
+        <CustomButton />
+      </ScrollView>
+      <Slot />
+    </KeyboardAvoidingView>
   )
 }
-
-export default _layout
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.elituvchi",
+  databaseId: '69fcc5dd00058e9688e0',
+  userCollectionId: '69fcc60a00109d645c26'
+};
